cdb11b35658344019c746c39fb699957a15e911d
Will Jordan <wajordan@vt.edu>
2021-04-25 20:31:52 -0400

    Implemented new grid + geometry features

--- src/geometry.f90
+++ src/geometry.f90
@@ -3,9 +3,10 @@ module geometry
   use set_precision,   only : prec
   use set_constants,   only : zero, one, two, half, pi
   use fluid_constants, only : R_gas, gamma
-  use set_inputs,      only : area, darea, eps
-  use soln_type
-  use grid_type
+  use set_inputs,      only : area, darea, eps, geometry_file, grid_name
+  use soln_type 
+  use grid_type,       only : grid_t, allocate_grid, deallocate_grid, &
+                              ghost_shape, cell_geometry
 
   implicit none
   
@@ -28,32 +29,19 @@ module geometry
   !===========================================================================80
   subroutine setup_geometry( grid, soln )
     
-    use set_inputs, only : i_low, i_high
+    use file_handling, only : grid_in, grid_out
     
     type( soln_t ), intent(inout) :: soln
     type( grid_t ), intent(inout) :: grid
     
     !integer :: i
     
-    !call allocate_grid( grid )
     !call allocate_soln( soln )
     
-    !do i = i_low-1,i_high
-    !  grid%Ai(i) = area( grid%xi(i) )
-    !end do
-    
-    !do i = i_low,i_high
-    !  grid%Ac(i) = area( grid%xc(i) )
-    !end do
-    
-    !do i = i_low,i_high
-    !  grid%dAc(i) = darea( grid%xc(i) )
-    !end do
-    !grid%dAc(ig_low:i_low-1) = zero
-    !do i = i_low,i_high
-    !  grid%dAc(i) = ( grid%Ai(i) - grid%Ai(i-1) )/grid%dx
-    !end do
-    !grid%dAc(i_high+1:ig_high) = zero
+    call grid_in(grid_name,grid)
+    call ghost_shape(grid)
+    call cell_geometry(grid)
+    call grid_out(geometry_file,grid)
     
   end subroutine setup_geometry
   
@@ -74,7 +62,7 @@ module geometry
     type( grid_t ) :: grid
     
     call deallocate_grid( grid )
-    call deallocate_soln( soln )
+    !call deallocate_soln( soln )
     
   end subroutine teardown_geometry
 
