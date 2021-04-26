1d9980c919f1db72e8d6ee985a432ca258567354
Will Jordan <wajordan@vt.edu>
2021-04-26 12:20:43 -0400

    removed Astar, area, darea, iSS, shock, and ramp commented out allocate statements in set_derived_inputs Added j indices Added isMMS variable (replaces "shock" variable)

--- src/set_inputs.f90
+++ src/set_inputs.f90
@@ -10,24 +10,26 @@ module set_inputs
 
   public :: imax, neq, xmin, xmax, n_ghost
   public :: i_high, i_low, ig_high, ig_low
-  public :: Astar, area, darea
+  public :: j_high, j_low, jg_high, jg_low
   public :: CFL, k2, k4, eps, tol, eps_roe, beta_lim, epsM, kappaM
   public :: max_iter, max_newton_iter, newton_tol, counter
-  public :: iSS, shock, ramp, soln_save, res_save, res_out
+  public :: isMMS, soln_save, res_save, res_out
   public :: p0, T0, a0, rho0, pb, p_ratio
   public :: set_derived_inputs, flux_scheme, limiter_scheme, cons
   public :: leftV, rightV, leftU, rightU, limiter_freeze, psi_plus, psi_minus
   public :: grid_name, geometry_file
    
-  integer :: imax    = 128
-  integer :: i_low   = 10
-  integer :: i_high  = 10
-  integer :: ig_low  = 10
-  integer :: ig_high = 10
-  integer :: neq  = 3
-  integer :: iSS  = 1
-  integer :: shock = 0
-  integer :: ramp = 0
+  integer :: imax    = 0
+  integer :: i_low   = 0
+  integer :: i_high  = 0
+  integer :: ig_low  = 0
+  integer :: ig_high = 0
+  integer :: jmax    = 0
+  integer :: j_low   = 0
+  integer :: j_high  = 0
+  integer :: jg_low  = 0
+  integer :: jg_high = 0
+  integer :: neq  = 4
   integer :: max_iter = 150000
   integer :: n_ghost   = 2
   integer :: max_newton_iter = 1000
@@ -58,6 +60,7 @@ module set_inputs
   real(prec) :: kappaM     = -one
   logical :: limiter_freeze = .false.
   logical :: cons           = .true.
+  logical :: isMMS = .false.
   real(prec), dimension(:,:), allocatable :: leftV, rightV, leftU, rightU
   real(prec), dimension(:,:), allocatable :: psi_plus, psi_minus
   integer :: counter = 1
@@ -66,43 +69,6 @@ module set_inputs
   !character(64) :: grid_name = "../grids/NACA64A006-grids/NACA64A006.fine.385x105.grd"
   character(64) :: geometry_file = "example.dat"
   contains
-
-  !=================================== area ==================================80
-  !>
-  !! Description: Calculates area distribution for nozzle.
-  !!
-  !! Inputs:      x:   Position coordinate along x-axis.
-  !!
-  !! Outputs:     area: Area at specified coordinate.
-  !<
-  !===========================================================================80
-  function area(x)
-
-    real(prec) :: area
-    real(prec), intent(in) :: x
-
-    area = 0.2_prec + 0.4_prec*( one + sin(pi*(x-0.5_prec)))
-
-  end function area
-  
-
-  !=================================== darea =================================80
-  !>
-  !! Description: Calculates area derivative distribution for nozzle.
-  !!
-  !! Inputs:      x:   Position coordinate along x-axis.
-  !!
-  !! Outputs:     darea: analytic dA/dx evaluated at specified coordinate.
-  !<
-  !===========================================================================80
-  function darea(x)
-
-    real(prec) :: darea
-    real(prec), intent(in) :: x
-
-    darea = 0.4_prec*pi*cos(pi*(x-0.5_prec))
-
-  end function darea
 
   !=========================== set_derived_inputs ============================80
   !>
@@ -113,10 +79,10 @@ module set_inputs
     
     a0   = sqrt(gamma*R_gas*T0)
     rho0 = 1000.0_prec*p0/(R_gas*T0)
-    i_low = 1
-    i_high = imax
-    ig_low  = 1 - n_ghost
-    ig_high = imax + n_ghost
+    !i_low = 1
+    !i_high = imax
+    !ig_low  = 1 - n_ghost
+    !ig_high = imax + n_ghost
     pb = p_ratio*p0*1000_prec
     write(*,'(A8,F20.14,A13)') 'R     = ', R_gas, ' [J/(kmol*K)]'
     write(*,'(A8,F20.14)')     'gamma = ', gamma
@@ -126,14 +92,12 @@ module set_inputs
     write(*,'(A8,F20.14,A4)')  'T_0   = ', T0, ' [K]'
     write(*,'(A8,F20.14,A6)')  'A*    = ', Astar, ' [m^2]'
 
-    allocate(leftV(i_low-1:i_high,1:neq))
-    allocate(rightV(i_low-1:i_high,1:neq))
-    allocate(leftU(i_low-1:i_high,1:neq))
-    allocate(rightU(i_low-1:i_high,1:neq))
-    !allocate(psi_plus(i_low-1:i_high,1:neq))
-    !allocate(psi_minus(i_low-1:i_high,1:neq))
-    allocate(psi_plus(ig_low:ig_high,1:neq))
-    allocate(psi_minus(ig_low:ig_high,1:neq))
+    !allocate(leftV(i_low-1:i_high,1:neq))
+    !allocate(rightV(i_low-1:i_high,1:neq))
+    !allocate(leftU(i_low-1:i_high,1:neq))
+    !allocate(rightU(i_low-1:i_high,1:neq))
+    !allocate(psi_plus(ig_low:ig_high,1:neq))
+    !allocate(psi_minus(ig_low:ig_high,1:neq))
     
   end subroutine set_derived_inputs
 
