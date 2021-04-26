e4e17b098adebab638625b785eace291540c29d4
Will Jordan <wajordan@vt.edu>
2021-04-26 12:18:48 -0400

    removed unused module use statements and variables added explicit use calls for grid_type and solution type

--- src/main_program.f90
+++ src/main_program.f90
@@ -2,43 +2,25 @@ program main_program
   
   use set_constants, only : set_derived_constants
   use fluid_constants, only : set_fluid_constants
-  use set_inputs, only : set_derived_inputs, grid_name
-  !use set_inputs, only : max_iter, tol, soln_save, res_save
-  !use set_inputs, only : leftV, rightV, leftU, rightU, flux_scheme
-  !use set_inputs, only : limiter_freeze, res_out, cons
-  !use variable_conversion
-  !use time_integration
-  !use boundary_conds, only : enforce_bndry
-  !use limiter_calc, only : select_limiter
-  !use flux_calc, only : select_flux, flux_fun
-  use other_subroutines
+  use set_inputs, only : set_derived_inputs
   use file_handling, only : grid_in, grid_out
   use geometry, only : setup_geometry, teardown_geometry
   !use init_problem, only : initialize
-  use namelist, only : read_namelist
-  use grid_type, only : grid_t, allocate_grid, deallocate_grid, &
-                        ghost_shape, cell_geometry
-  use soln_type
+  !use namelist, only : read_namelist
+  use grid_type, only : grid_t
+  use soln_type, only : soln_t
   implicit none
   
-  character(len=100) :: header_str1
-  character(len=100) :: niter
-  integer :: j, pnorm
+  !character(len=100) :: header_str1
+  !character(len=100) :: niter
+  !integer :: j, pnorm
   type( grid_t )      :: grid
   type( soln_t )      :: soln
-  
-  !open(50,file='temp.txt',status='unknown')
   
   call set_derived_constants
   call set_fluid_constants
   !call read_namelist('input.nml')
   call set_derived_inputs
-  !call allocate_grid( grid )
-  !call grid_in(grid_name,grid)
-  !call ghost_shape(grid)
-  !call cell_geometry(grid)
-  !call grid_out('example.dat',grid)
-  !call deallocate_grid( grid )
   call setup_geometry(grid,soln)
   
   
