9e69f77526ef20fb596b9d8aabc284bd736b5647
Will Jordan <wajordan@vt.edu>
2021-04-28 21:09:00 -0400

    updated soln and residual output files for 2D data made private, fixed a few rank mismatches

--- src/functions/other_subroutines.f90
+++ src/functions/other_subroutines.f90
@@ -2,8 +2,9 @@ module other_subroutines
   
   use set_precision, only : prec
   use set_constants, only : zero, one, two, three, half, fourth
-  use set_inputs, only : imax, neq, i_low, i_high, ig_low, ig_high, shock
-  use set_inputs, only : epsM, kappaM
+  use set_inputs, only : imax, neq, i_low, i_high, ig_low, ig_high
+  use set_inputs, only : jmax, j_low, j_high, jg_low, jg_high
+  use set_inputs, only : epsM, kappaM, isMMS
   use fluid_constants, only : gamma
   use variable_conversion
   use limiter_calc, only : limiter_fun, calc_consecutive_variations
@@ -11,6 +12,10 @@ module other_subroutines
   use grid_type, only : grid_t
   
   implicit none
+  
+  private
+  
+  public :: output_file_headers, output_soln
   
   contains
   
@@ -92,8 +97,8 @@ module other_subroutines
     !left(i_high,:)   = V(i_high,:)
     !right(i_low-1,:) = V(i_low-1,:)
     !right(i_high,:)  = V(i_high,:)
-    call limit_primitives(left)
-    call limit_primitives(right)
+    !call limit_primitives(left)
+    !call limit_primitives(right)
     !write(*,*)
     !do i = i_low-1,i_high
     !  write(*,*) i, left(i,1), left(i,2), left(i,3), right(i,1), right(i,2), right(i,3)
@@ -118,31 +123,30 @@ module other_subroutines
     type(soln_t), intent(inout) :: soln
     type(soln_t), intent(in) :: exact_soln
     logical, intent(in) :: cons
-    real(prec), dimension(i_low:i_high,1:neq), intent(out) :: DE
+    real(prec), dimension(:,:,:), intent(out) :: DE
     real(prec), dimension(1,1:neq), intent(out) :: DEnorm
     integer, intent(in) :: pnorm
-    real(prec) :: Linv
-    Linv = one/real(i_high-i_low)
+    real(prec) :: Linvi, Linvj
+    Linvi = one/real(i_high-i_low)
+    Linvj = one/real(j_high-j_low)
     if (cons) then
-      DE = soln%U(i_low:i_high,1:neq) - zero
+      DE = soln%U(i_low:i_high,j_low:j_high,1:neq) &
+       & - soln%Umms(i_low:i_high,j_low:j_high,1:neq)
     else
-      DE = soln%V(i_low:i_high,1:neq) - zero
+      DE = soln%V(i_low:i_high,j_low:j_high,1:neq) &
+       & - soln%Vmms(i_low:i_high,j_low:j_high,1:neq)
     end if
     
     if (pnorm == 0) then
-      DEnorm(1,1:neq) = maxval(abs(DE),1)
+      DEnorm(1,1) = Linvj*maxval(sum(abs(DE(:,:,1)),2))
+      DEnorm(1,2) = Linvj*maxval(sum(abs(DE(:,:,2)),2))
+      DEnorm(1,3) = Linvj*maxval(sum(abs(DE(:,:,3)),2))
+      DEnorm(1,4) = Linvj*maxval(sum(abs(DE(:,:,4)),2))
     elseif (pnorm == 1) then
-      DEnorm(1,1) = Linv*sum(abs(DE(:,1)),1)
-      DEnorm(1,2) = Linv*sum(abs(DE(:,2)),1)
-      DEnorm(1,3) = Linv*sum(abs(DE(:,3)),1)
-    elseif (pnorm == 2) then
-      DEnorm(1,1) = sqrt(Linv*sum(DE(:,1)**2,1))
-      DEnorm(1,2) = sqrt(Linv*sum(DE(:,2)**2,1))
-      DEnorm(1,3) = sqrt(Linv*sum(DE(:,3)**2,1))
-    else
-      DEnorm(1,1) = maxval(abs(DE(:,1)))
-      DEnorm(1,2) = maxval(abs(DE(:,2)))
-      DEnorm(1,3) = maxval(abs(DE(:,3)))
+      DEnorm(1,1) = Linvi*maxval(sum(abs(DE(:,:,1)),1))
+      DEnorm(1,2) = Linvi*maxval(sum(abs(DE(:,:,2)),1))
+      DEnorm(1,3) = Linvi*maxval(sum(abs(DE(:,:,3)),1))
+      DEnorm(1,4) = Linvi*maxval(sum(abs(DE(:,:,4)),1))
     end if
     
   end subroutine calc_de
@@ -158,45 +162,39 @@ module other_subroutines
   !===========================================================================80
   subroutine output_file_headers
     
-    use set_inputs, only : imax, shock, p_ratio
-    use set_inputs, only : flux_scheme, limiter_scheme, beta_lim
-    use set_inputs, only : CFL, k2, k4, epsM
+    use set_inputs, only : flux_scheme, limiter_scheme, beta_lim, cfl
+    use set_inputs, only : grid_name
     
     character(len=1024) :: dirname_hist
     character(len=1024) :: dirname_field
     character(len=1024) :: filename
     character(len=64) ::   ncells_str
-    character(len=64) ::   shock_str
+    character(len=64) ::   MMS_str
     character(len=64) ::   CFL_str
     character(len=64) ::   flux_str
     character(len=64) ::   order_str
     character(len=64) ::   limiter_str
     character(len=64) ::   kappa_str
-    character(len=64) ::   kappa2_str
-    character(len=64) ::   kappa4_str
+    integer :: resunit, fldunit
+    
+    resunit = 30
+    fldunit = 40
     
     ! Set up output directories
-    write (ncells_str  , "(A1,I0.3,A1)") "N"  , imax   , "_"
-    if (shock.eq.1) then
-      write (shock_str, "(A16,I0.2)") "normal-shock-pb-",int(100*p_ratio)
-    else
-      write (shock_str, "(A10)") "isentropic"
-    end if
-    write (CFL_str  , "(A4,I0.3)") "CFL-", int(1000*cfl)
+    write (ncells_str  , "(A5,I0.3,A6,I0.3)") "imax=",imax,"_jmax=",jmax
+    write (CFL_str  , "(A4,F0.4)") "CFL=",cfl
     
     select case(flux_scheme)
     case(1)
-      write (flux_str,"(A3)") "CTR"
-    case(2)
       write (flux_str,"(A3)") "V-L"
-    case(3)
+    case(2)
       write (flux_str,"(A3)") "ROE"
     case default
     end select
     if (nint(epsM).eq.0) then
-      write (order_str,"(A1)") "1"
+      write (order_str,"(A9)") "1st-order"
     else
-      write (order_str,"(A1)") "2"
+      write (order_str,"(A9)") "2nd-order"
     end if
     
     select case(limiter_scheme)
@@ -212,71 +210,51 @@ module other_subroutines
     end select
     
     write (kappa_str, "(A2,I3.2,A1)") "_K"  , int(10*kappaM),"_"
-    write (kappa2_str, "(A4,I0.4)") "_K2-"  , int(1000*k2)
-    write (kappa4_str, "(A4,I0.4)") "_K4-"  , int(1000*k4)
-    write (dirname_hist, *) adjustl(trim(shock_str)),"/hist/"
-    write (dirname_field, *) adjustl(trim(shock_str)),"/field/"
-    if (flux_scheme.eq.1) then
-      write (filename,*)  trim(ncells_str)//  &
-      &                   trim(flux_str)//    &
-      &                   trim(kappa2_str)//  &
-      &                   trim(kappa4_str)//  &
-      &                   trim(CFL_str)
-    else
-      write (filename,*)  trim(ncells_str)//  &
-      &                   trim(flux_str)//    &
-      &                   trim(order_str)//   &
-      &                   trim(limiter_str)// &
-      &                   trim(kappa_str)//   &
-      &                   trim(CFL_str)
-    end if 
+    write (dirname_hist, *) adjustl(trim(MMS_str)),"/hist/"
+    write (dirname_field, *) adjustl(trim(MMS_str)),"/field/"
+    !write (filename,*)  trim(ncells_str)//  &
+    !&                   trim(flux_str)//    &
+    !&                   trim(order_str)//   &
+    !&                   trim(limiter_str)// &
+    !&                   trim(kappa_str)//   &
+    !&                   trim(CFL_str)
     
-    call execute_command_line ('mkdir -p ../results/' // &
-    & adjustl(trim(dirname_hist)))
+    !call execute_command_line ('mkdir -p ../results/' // &
+    !& adjustl(trim(dirname_hist)))
     
-    call execute_command_line ('mkdir -p ../results/' // &
-    & adjustl(trim(dirname_field)))
+    !call execute_command_line ('mkdir -p ../results/' // &
+    !& adjustl(trim(dirname_field)))
     ! Set up output files (history and solution)
     !open(30,file='history.dat',status='unknown')
-    open(30,file= '../results/'//trim(adjustl(dirname_hist))//  &
-    &               trim(adjustl(filename))//'_history.dat',status='unknown')
-    write(30,*) 'TITLE = "Q1D res hist ('// &
-    & trim(adjustl(filename))//')"'
-    if(shock.eq.0) then
-      write(30,*) 'variables="Iteration""R<sub>1</sub>""R<sub>2</sub>"&
-      & "R<sub>3</sub>""DE<sub>1</sub>""DE<sub>2</sub>""DE<sub>3</sub>"'
-    else
-      write(30,*) 'variables="Iteration""Res1""Res2""Res3"'
-    end if
+    !open(resunit,file= '../results/'//trim(adjustl(dirname_hist))//  &
+    !&               trim(adjustl(filename))//'_history.dat',status='unknown')
+!    if(isMMS) then
+!      write(resunit,*) 'TITLE = "MMS ('// &
+!      & trim(adjustl(grid_name))//')"'
+!      write(resunit,*) 'variables="Iteration"   &
+!      &   "R<sub>1</sub>""R<sub>2</sub>"   &
+!      &   "R<sub>3</sub>""R<sub>4</sub>"   &
+!      &   "DE<sub>1</sub>""DE<sub>2</sub>" &
+!      &   "DE<sub>3</sub>""DE<sub>4</sub>"'
+!    else
+!      write(resunit,*) 'TITLE = "'// &
+!      & trim(adjustl(grid_name))//'"'
+!      write(resunit,*) 'variables="Iteration"   &
+!      &   "R<sub>1</sub>""R<sub>2</sub>"   &
+!      &   "R<sub>3</sub>""R<sub>4</sub>"'
+!    end if
     
-    open(40,file= '../results/'//trim(adjustl(dirname_field))//  &
-    &               trim(adjustl(filename))//'_field.dat',status='unknown')
-    write(40,*) 'TITLE = "Q1D soln ('// &
-    & trim(adjustl(filename))//')"'
-    if(shock.eq.0) then
-      write(40,*) 'variables="x(m)""A(m^2)""rho(kg/m^3)""u(m/s)""p(N/m^2)"  &
-      & "M""U1""U2""U3""M-exact""rho-exact""u-exact""p-exact""DE1""DE2""DE3"'
-      write(40,*) 'variables="x (m)""A (m<sup>2</sup>)"&
-      & "<greek>r</greek> (kg/m<sup>3</sup>)""u (m/s)"&
-      & "p (N/m<sup>2</sup>)"  &
-      & "M""U<sub>1</sub>""U<sub>2</sub>""U<sub>3</sub>"&
-      & "M<sub>exact</sub>""<greek>r</greek><sub>exact</sub>"&
-      & "u<sub>exact</sub>""p<sub>exact</sub>"&
-      & "DE<sub>1</sub>""DE<sub>2</sub>""DE<sub>3</sub>"'
-      !write(40,*) 'variables="x (m)""<greek>r</greek> (kg/m<sup>3</sup>)"&
-      !&"u (m/s)""p (N/m<sup>2</sup>)""M"&
-      !& "M<sub>exact</sub>""<greek>r</greek><sub>exact</sub>"&
-      !& "u<sub>exact</sub>""p<sub>exact</sub>"'
-    elseif(shock.eq.1) then
-      !write(40,*) 'variables="x (m)""A (m<sup>2</sup>)"&
-      !& "<greek>r</greek> (kg/m<sup>3</sup>)""u (m/s)"&
-      !& "p (N/m<sup>2</sup>)"&
-      !&"M""U<sub>1</sub>""U<sub>2</sub>""U<sub>3</sub>"'
-      write(40,*) 'variables="x (m)""<greek>r</greek> (kg/m<sup>3</sup>)"&
-      & "u (m/s)""p (N/m<sup>2</sup>)""M"'
+    open(fldunit,file= '../results/example_out.dat')
+!    open(fldunit,file= '../results/'//trim(adjustl(dirname_field))//  &
+!    &               trim(adjustl(filename))//'_field.dat',status='unknown')
+    write(fldunit,*) 'TITLE = "blahblahblah"'
+    if(isMMS) then
+      write(fldunit,*) 'variables="x""y""<greek>r</greek>""u""v""p""M"&
+      & "<greek>r</greek><sub>exact</sub>"&
+      & "u<sub>exact</sub>""v<sub>exact</sub>""p<sub>exact</sub>"&
+      & "DE<sub>1</sub>""DE<sub>2</sub>""DE<sub>3</sub>""DE<sub>4</sub>"'
     else
-      write(*,*) 'ERROR! shock must equal 0 or 1!!!'
-      stop
+      write(fldunit,*) 'variables="x""y""<greek>r</greek>""u""v""p""M"'
     endif
     
   end subroutine output_file_headers
@@ -290,49 +268,62 @@ module other_subroutines
   !!              num_iter : 
   !<
   !===========================================================================80
-  subroutine output_soln(grid,soln,ex_soln,num_iter)
+  subroutine output_soln(grid,soln,num_iter)
     
     use set_inputs, only : counter
     
     type( grid_t ), intent(in) :: grid
     type( soln_t ), intent(in) :: soln
-    type( soln_t ), intent(in) :: ex_soln
     integer,        intent(in) :: num_iter
     
-    integer :: i
+    integer :: i, j, fldunit
+    fldunit = 40
     
-    open(40,status='unknown')
+    open(fldunit,status='unknown')
     ! Repeat the following each time you want to write out the solution
     !write(40,*) 'zone T="',num_iter,'" '
-    write(40,*) 'zone T="',counter,'" '
-    write(40,*) 'I=',imax
-    if(shock.eq.0) then
-      write(40,*) 'DATAPACKING=POINT'
-      write(40,*) 'DT=(DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE &
-             & DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE &
-             & DOUBLE)'
-      !write(40,*) 'DT=(DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE &
-      !       & DOUBLE DOUBLE)'
-      do i = i_low,i_high
-        write(40,*) grid%x(1,i),grid%A_eta(1,i),soln%V(i,1),soln%V(i,2),soln%V(i,3),&
-             & soln%mach(i),soln%U(i,1),soln%U(i,2),soln%U(i,3), &
-             & ex_soln%mach(i), ex_soln%V(i,1), ex_soln%V(i,2),  &
-             & ex_soln%V(i,3), soln%DE(i,1), soln%DE(i,2), soln%DE(i,3)
-        !write(40,*) grid%xc(i),soln%V(i,1),soln%V(i,2),soln%V(i,3),&
-        !     & soln%mach(i), ex_soln%Mc(i), ex_soln%Vc(i,1), ex_soln%Vc(i,2),&
-        !     & ex_soln%Vc(i,3)
-      end do
-    elseif(shock.eq.1) then
-      write(40,*) 'DATAPACKING=POINT'
-      !write(40,*) 'DT=(DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE &
-      !       & DOUBLE DOUBLE)'
-      write(40,*) 'DT=(DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE)'
-      do i = i_low,i_high
-        !write(40,*) grid%xc(i),grid%Ac(i),soln%V(i,1),soln%V(i,2),soln%V(i,3),&
-        !     & soln%mach(i),soln%U(i,1),soln%U(i,2),soln%U(i,3)
-        write(40,*) grid%x(1,i),soln%V(i,1),soln%V(i,2),soln%V(i,3),&
-             & soln%mach(i)
-      end do
+    write(fldunit,*) 'ZONE'
+    write(fldunit,*) 'T= "',counter,'"'
+    write(fldunit,*) 'I=',i_high-i_low+2,' J=',j_high-j_low+2
+    if(isMMS) then
+      write(fldunit,*) 'DT = (DOUBLE, DOUBLE, DOUBLE, DOUBLE,&
+                           &  DOUBLE, DOUBLE, DOUBLE, DOUBLE, &
+                           &  DOUBLE, DOUBLE, DOUBLE, DOUBLE, &
+                           &  DOUBLE, DOUBLE, DOUBLE)'
+      write(fldunit,*) 'DATAPACKING = BLOCK'
+      write(fldunit,*) 'VARLOCATION = ([3-15]=CELLCENTERED)'
+      
+      write(fldunit,*) ((grid%x(i,j),i=i_low,i_high+1), NEW_LINE('a'), j=j_low,j_high+1)
+      write(fldunit,*) ((grid%y(i,j),i=i_low,i_high+1), NEW_LINE('a'), j=j_low,j_high+1)
+      write(fldunit,*) ((soln%V(i,j,1),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%V(i,j,2),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%V(i,j,3),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%V(i,j,4),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%mach(i,j),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%Vmms(i,j,1),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%Vmms(i,j,2),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%Vmms(i,j,3),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%Vmms(i,j,4),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%DE(i,j,1),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%DE(i,j,2),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%DE(i,j,3),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      write(fldunit,*) ((soln%DE(i,j,4),i=i_low,i_high), NEW_LINE('a'), j=j_low,j_high)
+      
+    else
+      
+      write(fldunit,*) 'DT = (DOUBLE, DOUBLE, DOUBLE, DOUBLE,&
+                           &  DOUBLE, DOUBLE, DOUBLE)'
+      write(fldunit,*) 'DATAPACKING = BLOCK'
+      write(fldunit,*) 'VARLOCATION = ([3-7]=CELLCENTERED'
+      
+      write(fldunit,*) ((grid%x(i,j),i=i_low,i_high+1), j=j_low,j_high+1)
+      write(fldunit,*) ((grid%y(i,j),i=i_low,i_high+1), j=j_low,j_high+1)
+      write(fldunit,*) ((soln%V(i,j,1),i=i_low,i_high), j=j_low,j_high)
+      write(fldunit,*) ((soln%V(i,j,2),i=i_low,i_high), j=j_low,j_high)
+      write(fldunit,*) ((soln%V(i,j,3),i=i_low,i_high), j=j_low,j_high)
+      write(fldunit,*) ((soln%V(i,j,4),i=i_low,i_high), j=j_low,j_high)
+      write(fldunit,*) ((soln%mach(i,j),i=i_low,i_high), j=j_low,j_high)
+   
     endif
     
   end subroutine output_soln
@@ -348,15 +339,16 @@ module other_subroutines
   !===========================================================================80
   subroutine output_res(soln,num_iter)
     
-    !real(prec), dimension(neq), intent(in) :: rnorm
     type( soln_t), intent(in) :: soln
     integer, intent(in) :: num_iter
-    integer :: i
-    ! Repeat the following each time you want to write out the solution
-    if(shock.eq.0) then
-      write(30,*) num_iter,(soln%rnorm(i),i=1,neq),(soln%DEnorm(i),i=1,neq)
+    integer :: i, resunit
+    
+    resunit = 30
+    
+    if(isMMS) then
+      write(resunit,*) num_iter,(soln%rnorm(i),i=1,neq),(soln%DEnorm(i),i=1,neq)
     else
-      write(30,*) num_iter,(soln%rnorm(i),i=1,neq)
+      write(resunit,*) num_iter,(soln%rnorm(i),i=1,neq)
     end if
     
   end subroutine output_res
