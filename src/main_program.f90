2a142232cd16496d2452314cd3460ab4c688c329
Will Jordan <wajordan@vt.edu>
2021-04-28 21:10:34 -0400

    commented out flux function calls added call to MMS solution and solution output

--- src/main_program.f90
+++ src/main_program.f90
@@ -6,10 +6,11 @@ program main_program
   use set_inputs, only : i_low,i_high,j_low,j_high
   use file_handling, only : grid_in, grid_out
   use geometry, only : setup_geometry, teardown_geometry
+  use other_subroutines, only : output_file_headers, output_soln
   !use init_problem, only : initialize
   !use namelist, only : read_namelist
   use grid_type, only : grid_t
-  use soln_type, only : soln_t
+  use soln_type, only : soln_t, calc_mms
   use flux_calc
   implicit none
   
@@ -25,31 +26,33 @@ program main_program
   !call read_namelist('input.nml')
   call set_derived_inputs
   call setup_geometry(grid,soln)
-  
+  call calc_mms(grid,soln)
+  call output_file_headers
+  call output_soln(grid,soln,1)
   !! flux at i-1/2 & i+1/2
-  call calc_flux_2D(soln%U(i_low-1:i_high,j_low:j_high+1,:),&
-                    soln%U(i_low:i_high+1,j_low:j_high+1,:),&
-                    grid%n_xi_x(i_low:i_high+1,j_low:j_high+1),&
-                    grid%n_xi_y(i_low:i_high+1,j_low:j_high+1),&
-                    soln%F(i_low:i_high+1,j_low:j_high+1,:))
-  
-  grid%n_xi_x(i_low:i_high,j_low:j_high) = &
-         soln%F(i_low+1:i_high+1,j_low:j_high,1)*&
-         grid%A_xi(i_low+1:i_high+1,j_low:j_high)&
-        -soln%F(i_low:i_high,j_low:j_high,1)*&
-         grid%A_xi(i_low:i_high,j_low:j_high)
-  
-  call calc_flux_2D(soln%U(i_low:i_high+1,j_low-1:j_high,:),&
-                    soln%U(i_low:i_high+1,j_low:j_high+1,:),&
-                    grid%n_eta_x(i_low:i_high+1,j_low:j_high+1),&
-                    grid%n_eta_y(i_low:i_high+1,j_low:j_high+1),&
-                    soln%F(i_low:i_high+1,j_low:j_high+1,:))
-  
-  grid%n_xi_y(i_low:i_high,j_low:j_high) = &
-         soln%F(i_low:i_high,j_low+1:j_high+1,1)*&
-         grid%A_eta(i_low:i_high,j_low+1:j_high+1)&
-        -soln%F(i_low:i_high,j_low:j_high,1)*&
-         grid%A_eta(i_low:i_high,j_low:j_high)
+!  call calc_flux_2D(soln%U(i_low-1:i_high,j_low:j_high+1,:),&
+!                    soln%U(i_low:i_high+1,j_low:j_high+1,:),&
+!                    grid%n_xi_x(i_low:i_high+1,j_low:j_high+1),&
+!                    grid%n_xi_y(i_low:i_high+1,j_low:j_high+1),&
+!                    soln%F(i_low:i_high+1,j_low:j_high+1,:))
+!  
+!  grid%n_xi_x(i_low:i_high,j_low:j_high) = &
+!         soln%F(i_low+1:i_high+1,j_low:j_high,1)*&
+!         grid%A_xi(i_low+1:i_high+1,j_low:j_high)&
+!        -soln%F(i_low:i_high,j_low:j_high,1)*&
+!         grid%A_xi(i_low:i_high,j_low:j_high)
+!  
+!  call calc_flux_2D(soln%U(i_low:i_high+1,j_low-1:j_high,:),&
+!                    soln%U(i_low:i_high+1,j_low:j_high+1,:),&
+!                    grid%n_eta_x(i_low:i_high+1,j_low:j_high+1),&
+!                    grid%n_eta_y(i_low:i_high+1,j_low:j_high+1),&
+!                    soln%F(i_low:i_high+1,j_low:j_high+1,:))
+!  
+!  grid%n_xi_y(i_low:i_high,j_low:j_high) = &
+!         soln%F(i_low:i_high,j_low+1:j_high+1,1)*&
+!         grid%A_eta(i_low:i_high,j_low+1:j_high+1)&
+!        -soln%F(i_low:i_high,j_low:j_high,1)*&
+!         grid%A_eta(i_low:i_high,j_low:j_high)
   
   call grid_out(geometry_file,grid)
   call teardown_geometry(grid,soln)
