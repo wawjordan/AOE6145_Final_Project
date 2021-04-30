e02a951f34192c566b1dc5ad1a15460a73b295b2
Will Jordan <wajordan@vt.edu>
2021-04-29 20:30:09 -0400

    Added call to updated calc_flux_2D

--- src/main_program.f90
+++ src/main_program.f90
@@ -7,6 +7,7 @@ program main_program
   use file_handling, only : grid_in, grid_out
   use geometry, only : setup_geometry, teardown_geometry
   use other_subroutines, only : output_file_headers, output_soln
+  use time_integration
   !use init_problem, only : initialize
   !use namelist, only : read_namelist
   use grid_type, only : grid_t
@@ -27,15 +28,11 @@ program main_program
   call set_derived_inputs
   call setup_geometry(grid,soln)
   call calc_mms(grid,soln)
-  call output_file_headers
-  call output_soln(grid,soln,1)
+  !call output_file_headers
+  !call output_soln(grid,soln,1)
   !! flux at i-1/2 & i+1/2
-!  call calc_flux_2D(soln%U(i_low-1:i_high,j_low:j_high+1,:),&
-!                    soln%U(i_low:i_high+1,j_low:j_high+1,:),&
-!                    grid%n_xi_x(i_low:i_high+1,j_low:j_high+1),&
-!                    grid%n_xi_y(i_low:i_high+1,j_low:j_high+1),&
-!                    soln%F(i_low:i_high+1,j_low:j_high+1,:))
-!  
+  
+  call calc_flux_2D(soln%V,grid%n_xi,grid%n_eta,soln%F)
 !  grid%n_xi_x(i_low:i_high,j_low:j_high) = &
 !         soln%F(i_low+1:i_high+1,j_low:j_high,1)*&
 !         grid%A_xi(i_low+1:i_high+1,j_low:j_high)&
