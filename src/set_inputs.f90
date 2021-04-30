17cacc9e17e32f3034e30c88b637876f145149a3
Will Jordan <wajordan@vt.edu>
2021-04-29 20:30:58 -0400

    Re-enabled initialization for i_low, i_high, etc. Added cart_grid logical variable

--- src/set_inputs.f90
+++ src/set_inputs.f90
@@ -8,23 +8,23 @@ module set_inputs
 
   private
 
-  public :: imax, jmax, neq, xmin, xmax, n_ghost
+  public :: imax, jmax, neq, xmin, xmax, ymin, ymax, n_ghost
   public :: i_high, i_low, ig_high, ig_low
   public :: j_high, j_low, jg_high, jg_low
   public :: CFL, k2, k4, eps, tol, eps_roe, beta_lim, epsM, kappaM
   public :: max_iter, max_newton_iter, newton_tol, counter
-  public :: isMMS, soln_save, res_save, res_out
+  public :: isMMS, cart_grid, soln_save, res_save, res_out
   public :: p0, T0, a0, rho0, pb, p_ratio
   public :: set_derived_inputs, flux_scheme, limiter_scheme, cons
   public :: leftV, rightV, leftU, rightU, limiter_freeze, psi_plus, psi_minus
   public :: grid_name, geometry_file
    
-  integer :: imax    = 0
+  integer :: imax    = 17
+  integer :: jmax    = 17
   integer :: i_low   = 0
   integer :: i_high  = 0
   integer :: ig_low  = 0
   integer :: ig_high = 0
-  integer :: jmax    = 0
   integer :: j_low   = 0
   integer :: j_high  = 0
   integer :: jg_low  = 0
@@ -42,8 +42,10 @@ module set_inputs
   real(prec) :: Astar      = 0.2_prec
   real(prec) :: a0         = zero
   real(prec) :: rho0       = zero
-  real(prec) :: xmin       = -one
+  real(prec) :: xmin       = zero
   real(prec) :: xmax       = one
+  real(prec) :: ymin       = zero
+  real(prec) :: ymax       = one
   real(prec) :: CFL        = 0.1_prec
   real(prec) :: p_ratio    = 0.5_prec
   real(prec) :: pb         = 150000_prec
@@ -61,12 +63,13 @@ module set_inputs
   logical :: limiter_freeze = .false.
   logical :: cons           = .true.
   logical :: isMMS = .true.
+  logical :: cart_grid = .true.
   real(prec), dimension(:,:), allocatable :: leftV, rightV, leftU, rightU
   real(prec), dimension(:,:), allocatable :: psi_plus, psi_minus
   integer :: counter = 1
-  !character(64) :: grid_name = "../grids/curvilinear-grids/curv2d17.grd"
+  character(64) :: grid_name = "../grids/curvilinear-grids/curv2d17.grd"
   !character(64) :: grid_name = "../grids/NACA64A006-grids/NACA64A006.extra-coarse.27x14.grd"
-  character(64) :: grid_name = "../grids/NACA64A006-grids/NACA64A006.fine.385x105.grd"
+  !character(64) :: grid_name = "../grids/NACA64A006-grids/NACA64A006.fine.385x105.grd"
   character(64) :: geometry_file = "example.dat"
   contains
 
@@ -79,10 +82,16 @@ module set_inputs
     
     a0   = sqrt(gamma*R_gas*T0)
     rho0 = 1000.0_prec*p0/(R_gas*T0)
-    !i_low = 1
-    !i_high = imax
-    !ig_low  = 1 - n_ghost
-    !ig_high = imax + n_ghost
+    i_low = 1
+    j_low = 1
+    i_high = imax-1
+    j_high = jmax-1
+    ig_low  = i_low - n_ghost
+    jg_low  = j_low - n_ghost
+    ig_high = i_high + n_ghost
+    jg_high = j_high + n_ghost
+    
+    
     pb = p_ratio*p0*1000_prec
     write(*,'(A8,F20.14,A13)') 'R     = ', R_gas, ' [J/(kmol*K)]'
     write(*,'(A8,F20.14)')     'gamma = ', gamma
