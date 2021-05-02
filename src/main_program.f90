99724d9493c0232bd6445910a1ed9ca03c60e788
Will Jordan <wajordan@vt.edu>
2021-05-02 13:06:06 -0400

    more debugging shit

--- src/main_program.f90
+++ src/main_program.f90
@@ -1,56 +1,53 @@
 program main_program
   
-  use set_constants, only : set_derived_constants
+  use set_constants, only : zero, set_derived_constants
   use fluid_constants, only : set_fluid_constants
   use set_inputs, only : set_derived_inputs, geometry_file
-  use set_inputs, only : i_low,i_high,j_low,j_high
+  use set_inputs, only : i_low,i_high,j_low,j_high, n_ghost
   use file_handling, only : grid_in, grid_out
   use geometry, only : setup_geometry, teardown_geometry
   use other_subroutines, only : output_file_headers, output_soln
   use time_integration
+  use limiter_calc, only : select_limiter, calc_consecutive_variations, limiter_fun
   !use init_problem, only : initialize
   !use namelist, only : read_namelist
   use grid_type, only : grid_t
   use soln_type, only : soln_t, calc_mms
-  use flux_calc
+  use flux_calc, only : select_flux, calc_flux_2D
   implicit none
   
   !character(len=100) :: header_str1
   !character(len=100) :: niter
-  !integer :: j, pnorm
+  integer :: j
   type( grid_t )      :: grid
   type( soln_t )      :: soln  
-
+  real(prec), dimension(20,4) :: r_plus, r_minus, psi_plus, psi_minus
+  r_plus = zero
+  r_minus =  zero
+  psi_plus = zero
+  psi_minus =  zero
   call set_derived_constants
   call set_fluid_constants
   call select_flux()
+  call select_limiter()
   !call read_namelist('input.nml')
   call set_derived_inputs
   call setup_geometry(grid,soln)
   call calc_mms(grid,soln)
   soln%V(i_low:i_high,j_low:j_high,:) = soln%Vmms
+  write(*,*)
+  write(*,*) lbound(soln%V,1), ubound(soln%V,1)
+  write(*,*) lbound(soln%V,2), ubound(soln%V,2)
+  write(*,*)
+  do j = j_low,j_high
+    write(*,*)'V',j,soln%V(i_low,j,:)
+  end do
+  write(*,*)
+  call calc_flux_2D(soln%V,grid%n_xi,grid%n_eta,soln%F)
+  !write(*,*)'Vmms', soln%Vmms(i_low,j_low,:)
+  !write(*,*)'V', soln%V(i_low,j_low,:)
   !call output_file_headers
   !call output_soln(grid,soln,1)
-  !! flux at i-1/2 & i+1/2
-  
-  call calc_flux_2D(soln%V,grid%n_xi,grid%n_eta,soln%F)
-!  grid%n_xi_x(i_low:i_high,j_low:j_high) = &
-!         soln%F(i_low+1:i_high+1,j_low:j_high,1)*&
-!         grid%A_xi(i_low+1:i_high+1,j_low:j_high)&
-!        -soln%F(i_low:i_high,j_low:j_high,1)*&
-!         grid%A_xi(i_low:i_high,j_low:j_high)
-!  
-!  call calc_flux_2D(soln%U(i_low:i_high+1,j_low-1:j_high,:),&
-!                    soln%U(i_low:i_high+1,j_low:j_high+1,:),&
-!                    grid%n_eta_x(i_low:i_high+1,j_low:j_high+1),&
-!                    grid%n_eta_y(i_low:i_high+1,j_low:j_high+1),&
-!                    soln%F(i_low:i_high+1,j_low:j_high+1,:))
-!  
-!  grid%n_xi_y(i_low:i_high,j_low:j_high) = &
-!         soln%F(i_low:i_high,j_low+1:j_high+1,1)*&
-!         grid%A_eta(i_low:i_high,j_low+1:j_high+1)&
-!        -soln%F(i_low:i_high,j_low:j_high,1)*&
-!         grid%A_eta(i_low:i_high,j_low:j_high)
   
   call grid_out(geometry_file,grid)
   call teardown_geometry(grid,soln)
