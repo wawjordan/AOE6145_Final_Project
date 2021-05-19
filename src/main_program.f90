10bf7cdfaae321095938d57ee5d6e4ec51ed0c1e
Will Jordan <wajordan@vt.edu>
2021-05-18 20:59:01 -0400

    many small changes... commented out/deleted many BC-specific lines in an attempt to figure that out added call to gauss_pts

--- src/main_program.f90
+++ src/main_program.f90
@@ -15,15 +15,16 @@ program main_program
   use grid_type, only : grid_t
   use soln_type, only : soln_t, calc_mms
   use flux_calc, only : select_flux, calc_flux_2D
+  use quadrature
   implicit none
   
   !character(len=100) :: header_str1
   !character(len=100) :: niter
-  integer :: i, j
+  integer :: i, j, k
   type( grid_t )      :: grid
   type( soln_t )      :: soln
   type( subsonic_mms_bc ) :: bndry1, bndry2, bndry3, bndry4
-  !real(prec), dimension(20,4) :: r_plus, r_minus, psi_plus, psi_minus
+  real(prec), dimension(3) :: xi, w
   integer, dimension(:,:), allocatable :: ind1, ind2, ind3, ind4
   
   !r_plus = zero
@@ -37,44 +38,56 @@ program main_program
   !call read_namelist('input.nml')
   call set_derived_inputs
   call setup_geometry(grid,soln)
-  
-  allocate(ind1(n_ghost*(i_high-i_low),2))
-  allocate(ind2(n_ghost*(i_high-i_low+1),2))
-  allocate(ind3(n_ghost*(j_high-j_low+1),2))
-  allocate(ind4(n_ghost*(j_high-j_low+1),2))
-write(*,*) ( ( i, i=i_low,i_high ), j=j_low-1,jg_low,-1)
-write(*,*) ( ( j, i=i_low,i_high ), j=j_low-1,jg_low,-1)
-write(*,*) lbound(ind1,1), ubound(ind1,1)
-write(*,*) lbound(ind1,2), ubound(ind1,2)
-do j = j_low-1,jg_low,-1
-do i = i_low,i_high
-ind1(i,1) = i
-ind1(i,2) = j
-write(*,*) ind1(i,:)
-end do
-end do
-!ind1(:,1) = [( ( i, i=i_low,i_high ), j=j_low-1,jg_low,-1)]
-!ind1(:,2) = [( ( j, i=i_low,i_high ), j=j_low-1,jg_low,-1)]
-!ind1(:,1) = [( ( i, i=i_low,i_high ), j=j_low-1,j_low-1)]
-!ind1(:,2) = [( ( j, i=i_low,i_high ), j=j_low-1,j_low-1)]
-!  
-!  ind2(:,1) = [( ( i, i=i_low,i_high ), j=j_high,jg_high)]
-!  ind2(:,2) = [( ( j, i=i_low,i_high ), j=j_high,jg_high)]
-!  
-!  ind3(:,1) = [( ( i, i=i_low,ig_low,-1 ), j=j_low,j_high)]
-!  ind3(:,2) = [( ( j, i=i_low,ig_low,-1 ), j=j_low,j_high)]
-!  
-!  ind4(:,1) = [( ( i, i=i_high,ig_high ), j=j_low,j_high)]
-!  ind4(:,2) = [( ( j, i=i_high,ig_high ), j=j_low,j_high)]
-  write(*,*) 'i_low= ',i_low
-  write(*,*) 'i_high= ',i_high
-  write(*,*) 'j_low= ',j_low
-  write(*,*) 'j_high= ',j_high
-  write(*,*)
+  call gauss_pts(xi,w,3)
+!  allocate(ind1(n_ghost*(i_high-i_low+1),2))
+!  allocate(ind2(n_ghost*(i_high-i_low+1),2))
+!  allocate(ind3(n_ghost*(j_high-j_low+1),2))
+!  allocate(ind4(n_ghost*(j_high-j_low+1),2))
+!write(*,*) ( ( i, i=i_low,i_high ), j=j_low-1,jg_low,-1)
+!write(*,*) ( ( j, i=i_low,i_high ), j=j_low-1,jg_low,-1)
+!write(*,*) lbound(ind1,1), ubound(ind1,1)
+!write(*,*) lbound(ind1,2), ubound(ind1,2)
+
+
+!do j = jg_low,j_low-1
+!do i = i_low,i_high
+!k = (j-jg_low)*n_ghost+i
+!ind1(k,1) = i
+!ind1(k,2) = j
+!write(*,*) ind1(i,:)
+!end do
+!end do
+!
+!do j = j_high+1,jg_high
+!do i = i_low,i_high
+!k = (j-j_high-1)*n_ghost+i
+!ind2(k,1) = i
+!ind2(k,2) = j
+!write(*,*) ind2(i,:)
+!end do
+!end do
+!
+!do j = j_low,j_high
+!do i = ig_low,i_low-1
+!k = (j-j_low)*n_ghost+(i-ig_low)+1
+!ind3(k,1) = i
+!ind3(k,2) = j
+!write(*,*) ind3(j,:)
+!end do
+!end do
+!
+!do j = j_low,j_high
+!do i = i_high+1,ig_high
+!k = (j-j_low)*n_ghost+(i-i_high)
+!ind4(k,1) = i
+!ind4(k,2) = j
+!write(*,*) ind4(j,:)
+!end do
+!end do
+
 !  do i = 1, size(ind1,1)
 !  write(*,*)'ind1', ind1(i,1),ind1(i,2)
 !  end do
-  write(*,*)
 !  do i = 1, size(ind2,1)
 !  write(*,*)'ind2', ind2(i,1),ind2(i,2)
 !  end do
@@ -87,15 +100,15 @@ end do
 !  write(*,*)'ind4', ind4(i,1),ind4(i,2)
 !  end do
   
-  call bndry1%init_mms_bc(ind1,grid) 
-  call bndry2%init_mms_bc(ind2,grid)
-  call bndry3%init_mms_bc(ind3,grid)
-  call bndry4%init_mms_bc(ind4,grid)
-  
-  call bndry1%enforce(soln)
-  call bndry2%enforce(soln)
-  call bndry3%enforce(soln)
-  call bndry4%enforce(soln)
+!  call bndry1%init_mms_bc(ind1,grid) 
+!  call bndry2%init_mms_bc(ind2,grid)
+!  call bndry3%init_mms_bc(ind3,grid)
+!  call bndry4%init_mms_bc(ind4,grid)
+!  
+!  call bndry1%enforce(soln)
+!  call bndry2%enforce(soln)
+!  call bndry3%enforce(soln)
+!  call bndry4%enforce(soln)
   
   call calc_mms(grid,soln)
   
@@ -114,7 +127,7 @@ end do
   call output_file_headers
   call output_soln(grid,soln,1)
   
-  deallocate(ind1,ind2,ind3,ind4)
+  !deallocate(ind1,ind2,ind3,ind4)
   call grid_out(geometry_file,grid)
   call teardown_geometry(grid,soln)
   close(50)
