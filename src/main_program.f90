3d6f53bb9c6dc7005ab58a160f570ba9233b8e94
Will Jordan <wajordan@vt.edu>
2021-05-20 15:39:18 -0400

    removed old debugging code, tried new tests

--- src/main_program.f90
+++ src/main_program.f90
@@ -23,14 +23,9 @@ program main_program
   integer :: i, j, k
   type( grid_t )      :: grid
   type( soln_t )      :: soln
-  type( subsonic_mms_bc ) :: bndry1, bndry2, bndry3, bndry4
-  !real(prec), dimension(3) :: xi, w
-  !integer, dimension(:,:), allocatable :: ind1, ind2, ind3, ind4
+  type( subsonic_mms_bc ) :: bndry1
+  integer, dimension(:), allocatable :: ind1, ind2
   
-  !r_plus = zero
-  !r_minus =  zero
- ! psi_plus = zero
- ! psi_minus =  zero
   call set_derived_constants
   call set_fluid_constants
   call select_flux()
@@ -38,87 +33,33 @@ program main_program
   !call read_namelist('input.nml')
   call set_derived_inputs
   call setup_geometry(grid,soln)
-!  allocate(ind1(n_ghost*(i_high-i_low+1),2))
-!  allocate(ind2(n_ghost*(i_high-i_low+1),2))
-!  allocate(ind3(n_ghost*(j_high-j_low+1),2))
-!  allocate(ind4(n_ghost*(j_high-j_low+1),2))
-!write(*,*) ( ( i, i=i_low,i_high ), j=j_low-1,jg_low,-1)
-!write(*,*) ( ( j, i=i_low,i_high ), j=j_low-1,jg_low,-1)
-!write(*,*) lbound(ind1,1), ubound(ind1,1)
-!write(*,*) lbound(ind1,2), ubound(ind1,2)
+  allocate(ind1(n_ghost*(i_high-i_low+1)))
+  allocate(ind2(n_ghost))
 
 
-!do j = jg_low,j_low-1
-!do i = i_low,i_high
-!k = (j-jg_low)*n_ghost+i
-!ind1(k,1) = i
-!ind1(k,2) = j
-!write(*,*) ind1(i,:)
-!end do
-!end do
-!
-!do j = j_high+1,jg_high
-!do i = i_low,i_high
-!k = (j-j_high-1)*n_ghost+i
-!ind2(k,1) = i
-!ind2(k,2) = j
-!write(*,*) ind2(i,:)
-!end do
-!end do
-!
-!do j = j_low,j_high
-!do i = ig_low,i_low-1
-!k = (j-j_low)*n_ghost+(i-ig_low)+1
-!ind3(k,1) = i
-!ind3(k,2) = j
-!write(*,*) ind3(j,:)
-!end do
-!end do
-!
-!do j = j_low,j_high
-!do i = i_high+1,ig_high
-!k = (j-j_low)*n_ghost+(i-i_high)
-!ind4(k,1) = i
-!ind4(k,2) = j
-!write(*,*) ind4(j,:)
-!end do
-!end do
-
-!  do i = 1, size(ind1,1)
-!  write(*,*)'ind1', ind1(i,1),ind1(i,2)
-!  end do
-!  do i = 1, size(ind2,1)
-!  write(*,*)'ind2', ind2(i,1),ind2(i,2)
-!  end do
-!  write(*,*)
-!  do i = 1, size(ind3,1)
-!  write(*,*)'ind3', ind3(i,1),ind3(i,2)
-!  end do
-!  write(*,*)
-!  do i = 1, size(ind4,1)
-!  write(*,*)'ind4', ind4(i,1),ind4(i,2)
-!  end do
+do j = jg_low,j_low-1
+ind2(j-jg_low+1) = j
+do i = i_low,i_high
+k = (j-jg_low)*(i_high-i_low+1)+i
+ind1(k) = i
+end do
+end do
   
-!  call bndry1%init_mms_bc(ind1,grid) 
-!  call bndry2%init_mms_bc(ind2,grid)
-!  call bndry3%init_mms_bc(ind3,grid)
-!  call bndry4%init_mms_bc(ind4,grid)
-!  
-!  call bndry1%enforce(soln)
-!  call bndry2%enforce(soln)
-!  call bndry3%enforce(soln)
-!  call bndry4%enforce(soln)
+  call bndry1%init_mms_bc(ind1,ind2,grid) 
+  call bndry1%enforce(soln)
   
   call calc_mms(grid,soln)
   
   !soln%V(i_low:i_high,j_low:j_high,:) = soln%Vmms(i_low:i_high,j_low:j_high,:)
   soln%V = soln%Vmms
+! stop 
   do i = 1,20
   call prim2cons(soln%U,soln%V)
   call calc_flux_2D(soln,grid,soln%F)
   call calc_time_step(grid%A_xi,grid%A_eta,grid%n_xi_avg, &
                       grid%n_eta_avg,grid%V,soln%V,soln%dt)
-  call explicit_RK(grid,soln%S,soln%dt,soln%F,soln%U,soln%R,1)
+  call bndry1%enforce(soln)
+  call explicit_RK(grid,soln%S,soln%dt,soln%F,soln%U,soln%R,4)
   call update_states(soln)
   end do
   !write(*,*)'Vmms', soln%Vmms(i_low,j_low,:)
@@ -133,7 +74,7 @@ program main_program
   call output_file_headers
   call output_soln(grid,soln,1)
   
-  !deallocate(ind1,ind2,ind3,ind4)
+  deallocate(ind1,ind2)
   !call grid_out(geometry_file,grid)
   call teardown_geometry(grid,soln)
   close(50)
