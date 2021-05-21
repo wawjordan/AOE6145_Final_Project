102b9e55d2db7fac2f9417f4aaca86453a4f442f
Will Jordan <wajordan@vt.edu>
2021-05-21 12:42:15 -0400

    added call to source term calculation

--- src/main_program.f90
+++ src/main_program.f90
@@ -6,7 +6,7 @@ program main_program
   use set_inputs, only : i_low,i_high,j_low,j_high, n_ghost
   use file_handling, only : grid_in, grid_out
   use geometry, only : setup_geometry, teardown_geometry
-  use other_subroutines, only : output_file_headers, output_soln
+  use other_subroutines
   use time_integration
   use limiter_calc, only : select_limiter, calc_consecutive_variations, limiter_fun
   !use init_problem, only : initialize
@@ -38,10 +38,12 @@ program main_program
   
   !soln%V(i_low:i_high,j_low:j_high,:) = soln%Vmms(i_low:i_high,j_low:j_high,:)
   soln%V = soln%Vmms
+  !soln%S = soln%Smms
 ! stop 
   do i = 1,20
   call prim2cons(soln%U,soln%V)
   call calc_flux_2D(soln,grid,soln%F)
+  !call calc_sources(soln,grid)
   call calc_time_step(grid%A_xi,grid%A_eta,grid%n_xi_avg, &
                       grid%n_eta_avg,grid%V,soln%V,soln%dt)
   call explicit_RK(grid,soln%S,soln%dt,soln%F,soln%U,soln%R,4)
