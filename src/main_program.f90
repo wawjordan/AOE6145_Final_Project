0e87d1165ef17b201cd04ae6ce439ac570ff1c3e
Will Jordan <wajordan@vt.edu>
2021-05-24 18:31:11 -0400

    Now uses cons variable Changed iteration loop to 100 iterations, with solution file output every 10 iterations

--- src/main_program.f90
+++ src/main_program.f90
@@ -3,7 +3,7 @@ program main_program
   use set_constants, only : zero, set_derived_constants
   use fluid_constants, only : set_fluid_constants
   use set_inputs, only : set_derived_inputs, geometry_file
-  use set_inputs, only : i_low,i_high,j_low,j_high, n_ghost
+  use set_inputs, only : i_low,i_high,j_low,j_high, n_ghost, cons
   use file_handling, only : grid_in, grid_out
   use geometry, only : setup_geometry, teardown_geometry
   use other_subroutines
@@ -35,13 +35,13 @@ program main_program
   call setup_geometry(grid,soln)
 
   call calc_mms(grid,soln)
-  
+  call output_exact_soln(grid,soln)
   call output_file_headers
   soln%V = soln%Vmms
   !soln%V(i_low:i_high,j_low:j_high,:) = soln%Vmms(i_low:i_high,j_low:j_high,:)
   soln%S = soln%Smms
 ! stop 
-  do i = 1,10
+  do i = 1,100
   call prim2cons(soln%U,soln%V)
   call calc_flux_2D(soln,grid,soln%F)
   !call calc_sources(soln,grid)
@@ -49,7 +49,10 @@ program main_program
                       grid%n_eta_avg,grid%V,soln%V,soln%dt)
   call explicit_RK(grid,soln%S,soln%dt,soln%F,soln%U,soln%R,1)
   call update_states(soln)
+  call calc_de(soln,soln%DE,soln%DEnorm,0,cons)
+  if (mod(i,10)==0) then
   call output_soln(grid,soln,i)
+  end if
   end do
   
   !call grid_out(geometry_file,grid)
