ee44d5c080fa00cbf2e97c69c5728498e63cc3b1
Will Jordan <wajordan@vt.edu>
2021-06-04 18:39:41 -0400

    Updated index ordering Changed residual norm to use all 3 norms (now that I think about it, this was probably stupid and unnecessary)

--- src/functions/time_integration.f90
+++ src/functions/time_integration.f90
@@ -35,17 +35,17 @@ module time_integration
     real(prec), dimension(:,:),   intent(out) :: dt
     integer :: low1, low2, high1, high2
     
-    low1  = lbound(V,1)
-    high1 = ubound(V,1)
-    low2  = lbound(V,2)
-    high2 = ubound(V,2)
+    low1  = lbound(V,2)
+    high1 = ubound(V,2)
+    low2  = lbound(V,3)
+    high2 = ubound(V,3)
 
 
-    call speed_of_sound(V(:,:,4),V(:,:,1),asnd)
-    lambda_xi  = abs(V(:,:,2)*n_xi_avg(:,:,1) + &
-                     V(:,:,3)*n_xi_avg(:,:,2)) + asnd
-    lambda_eta = abs(V(:,:,2)*n_eta_avg(:,:,1) + &
-                     V(:,:,3)*n_eta_avg(:,:,2)) + asnd
+    call speed_of_sound(V(4,:,:),V(1,:,:),asnd)
+    lambda_xi  = abs(V(2,:,:)*n_xi_avg(1,:,:) + &
+                     V(3,:,:)*n_xi_avg(2,:,:)) + asnd
+    lambda_eta = abs(V(2,:,:)*n_eta_avg(1,:,:) + &
+                     V(3,:,:)*n_eta_avg(2,:,:)) + asnd
     dt = CFL*vol/(lambda_xi*A_xi(low1:high1,low2:high2) + &
                   lambda_eta*A_eta(low1:high1,low2:high2))
     dt = CFL*minval(dt)
@@ -83,9 +83,9 @@ module time_integration
     !                  soln%F(i_low:i_high+1,j_low:j_high+1,:,:),&
     !                  soln%R(i_low:i_high,j_low:j_high,:))
     do i = 1,4
-    soln%U(i_low:i_high,j_low:j_high,i) = &
-         soln%U(i_low:i_high,j_low:j_high,i) - &
-       ( soln%R(i_low:i_high,j_low:j_high,i)*  &
+    soln%U(i,i_low:i_high,j_low:j_high) = &
+         soln%U(i,i_low:i_high,j_low:j_high) - &
+       ( soln%R(i,i_low:i_high,j_low:j_high)*  &
         soln%dt(i_low:i_high,j_low:j_high) )/  &
          grid%V(i_low:i_high,j_low:j_high)
     end do
@@ -134,14 +134,11 @@ module time_integration
     integer :: i,j
     do j = j_low,j_high
     do i = i_low,i_high
-      !R(i,j,:) = A_xi(i+1,j)*F(i+1,j,:,1) - A_xi(i,j)*F(i,j,:,1)  &
-      !         + A_eta(i,j+1)*F(i,j+1,:,2) - A_eta(i,j)*F(i,j,:,2) &
-      !         - V(i,j)*S(i,j,:)
-      soln%R(i,j,:) = grid%A_xi(i+1,j)*soln%F(i+1,j,:,1) &
-                    - grid%A_xi(i,j)*soln%F(i,j,:,1)  &
-                    + grid%A_eta(i,j+1)*soln%F(i,j+1,:,2) &
-                    - grid%A_eta(i,j)*soln%F(i,j,:,2) &
-                    - grid%V(i,j)*soln%S(i,j,:)
+      soln%R(i,j,:) = grid%A_xi(i+1,j)*soln%Fxi(:,i+1,j) &
+                    - grid%A_xi(i,j)*soln%Fxi(:,i,j)  &
+                    + grid%A_eta(i,j+1)*soln%Feta(:,i,j+1) &
+                    - grid%A_eta(i,j)*soln%Feta(:,i,j) &
+                    - grid%V(i,j)*soln%S(:,i,j)
     end do
     end do
   end subroutine calc_residual
@@ -205,33 +202,21 @@ module time_integration
   !===========================================================================80
   subroutine residual_norms( R, Rnorm, pnorm, rinit )
     
-    real(prec), dimension(i_low:i_high,j_low:j_high,neq), intent(in) :: R
-    real(prec), dimension(neq), intent(in)  :: rinit
-    real(prec), dimension(neq), intent(out) :: Rnorm
+    real(prec), dimension(neq,i_low:i_high,j_low:j_high), intent(in) :: R
+    real(prec), dimension(neq,3), intent(in)  :: rinit
+    real(prec), dimension(neq,3), intent(out) :: Rnorm
     integer, intent(in) :: pnorm
     real(prec) :: Linv
     integer :: i
     
-    Linv = one/real(size(R(:,:,1)),prec)
+    Linv = one/real(size(R(1,:,:)),prec)
     
-    if (pnorm == 0) then
-      do i = 1,neq
-        Rnorm(i) = maxval(abs(R(:,:,i)))
-      end do
-    elseif (pnorm == 1) then
-      do i = 1,neq
-        Rnorm(i) = Linv*sum(abs(R(:,:,i)))
-      end do
-    elseif (pnorm == 2) then
-      do i = 1,neq 
-        Rnorm(i) = sqrt(Linv*sum(R(:,:,i)**2))
-      end do
-    else
-      do i = 1,neq
-        Rnorm(i) = maxval(abs(R(:,:,i)))
-      end do
-    end if
-     Rnorm = Rnorm/rinit
+    do i = 1,neq
+      Rnorm(i,1) = maxval(abs(R(i,:,:)))
+      Rnorm(i,2) = Linv*sum(abs(R(i,:,:)))
+      Rnorm(i,3) = sqrt(Linv*sum(R(i,:,:)**2))
+    end do
+    Rnorm = Rnorm/rinit
      
   end subroutine residual_norms
 
