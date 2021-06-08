639047887ace13db5dc77385c7f5cbaddaef5e74
Will Jordan <wajordan@vt.edu>
2021-06-08 08:29:34 -0400

    re-enabled namelist module added call to read_namelist subroutine added temporary file for collecting final DEnorms and Rnorms Changed loop to use max_iter shifted indices for flux BC (hopefully correctly) added solution converged message

--- src/main_program.f90
+++ src/main_program.f90
@@ -5,8 +5,8 @@ program main_program
   use fluid_constants, only : set_fluid_constants
   use set_inputs, only : set_derived_inputs, geometry_file, limiter_freeze
   use set_inputs, only : i_low,i_high,j_low,j_high, n_ghost, cons, neq
-  use set_inputs, only : ig_low,ig_high,jg_low,jg_high
-  use set_inputs, only : res_save, res_out, soln_save, tol, Lmms, CFL
+  use set_inputs, only : ig_low,ig_high,jg_low,jg_high, max_iter
+  use set_inputs, only : res_save, res_out, soln_save, tol, Lmms, CFL, isMMS
   use file_handling, only : grid_out, output_file_headers, &
                             output_exact_soln, output_soln, output_res
   use geometry, only : setup_geometry, teardown_geometry
@@ -17,7 +17,7 @@ program main_program
   use limiter_calc, only : select_limiter, limiter_fun, calculate_limiters
   use mms_functions, only : rho_mms, uvel_mms, vvel_mms, press_mms
   use init_problem, only : initialize_MMS
-  !use namelist, only : read_namelist
+  use namelist, only : read_namelist
   use grid_type, only : grid_t
   use soln_type, only : soln_t, calc_mms
   use flux_calc, only : select_flux, calc_flux_2D, flux_fun
@@ -38,28 +38,29 @@ program main_program
   
   call set_derived_constants
   call set_fluid_constants
-  call select_flux()
-  call select_limiter()
+  call read_namelist('input.nml')
   call set_derived_inputs
   call setup_geometry(grid,soln)
+  call select_flux()
+  call select_limiter()
   call initialize_MMS(grid,soln)
   call output_exact_soln(grid,soln)
   call output_file_headers  
   call output_soln(grid,soln,0)
-!  !call calc_sources(soln,grid)
+  !call calc_sources(soln,grid)
   
+   open(42,file='temp.txt',status='unknown')
  
- 
-  do k = 1,100000
+  do k = 1,max_iter
 !!==============================================================================
-  !  soln%V(:,i_low:i_high,jg_high-1) = 2*soln%V(:,i_low:i_high,j_high  ) &
-  !                                     - soln%V(:,i_low:i_high,j_high-1)
-  !  soln%V(:,i_low:i_high,jg_high)   = 2*soln%V(:,i_low:i_high,j_high-1) &
-  !                                     - soln%V(:,i_low:i_high,j_high-2)
-  !  soln%V(:,ig_high-1,j_low:j_high) = 2*soln%V(:,i_high  ,j_low:j_high) &
-  !                                     - soln%V(:,i_high-1,j_low:j_high)
-  !  soln%V(:,ig_high,j_low:j_high)   = 2*soln%V(:,i_high-1,j_low:j_high) &
-  !                                     - soln%V(:,i_high-2,j_low:j_high)
+    !soln%V(:,i_low:i_high,jg_high-1) = 2*soln%V(:,i_low:i_high,j_high  ) &
+    !                                   - soln%V(:,i_low:i_high,j_high-1)
+    !soln%V(:,i_low:i_high,jg_high)   = 2*soln%V(:,i_low:i_high,j_high-1) &
+    !                                   - soln%V(:,i_low:i_high,j_high-2)
+    !soln%V(:,ig_high-1,j_low:j_high) = 2*soln%V(:,i_high  ,j_low:j_high) &
+    !                                   - soln%V(:,i_high-1,j_low:j_high)
+    !soln%V(:,ig_high,j_low:j_high)   = 2*soln%V(:,i_high-1,j_low:j_high) &
+    !                                   - soln%V(:,i_high-2,j_low:j_high)
      
     call limit_primitives(soln%V)
     call prim2cons(soln%U,soln%V)
@@ -69,38 +70,38 @@ program main_program
     !end if
     call calc_flux_2D(grid,soln)
   
-  !i1 = i_high
-  !do j1 = j_low,j_high
-  !  nx = grid%n_xi(i1,j1,1)
-  !  ny = grid%n_xi(i1,j1,2)
-  !  !ind = (/ ( k1,k1=i1-2,i1+1 ) /)
-  !  !Vtmp(1,:) = soln%V(ind(1),j1,:)
-  !  !Vtmp(2,:) = soln%Vmms(ind(2),j1,:)
-  !  !Vtmp(3,:) = soln%Vmms(ind(3),j1,:)
-  !  !Vtmp(4,:) = soln%Vmms(ind(4),j1,:)
-  !  !psiPtmp = soln%psi_plus(ind,j1,:,1)
-  !  !psiMtmp = soln%psi_minus(ind,j1,:,1)
-  !  !call MUSCL_extrap(Vtmp, psiPtmp, psiMtmp, left, right)
-  !  left = soln%V(:,i1,j1)
-  !  right = soln%Vmms(:,i1+1,j1)
-  !  call flux_fun(left,right,nx,ny,soln%Fxi(:,i1,j1))
-  !end do
-  !j1 = j_high
-  !do i1 = i_low,i_high
-  !  nx = grid%n_eta(i1,j1,1)
-  !  ny = grid%n_eta(i1,j1,2)
-  !  !ind = (/ ( k1,k1=j1-2,j1+1 ) /)
-  !  !Vtmp(1,:) = soln%V(i1,ind(1),:)
-  !  !Vtmp(2,:) = soln%V(i1,ind(2),:)
-  !  !Vtmp(3,:) = soln%V(i1,ind(3),:)
-  !  !Vtmp(4,:) = soln%Vmms(i1,ind(4),:)
-  !  !psiPtmp = soln%psi_plus(i1,ind,:,2)
-  !  !psiMtmp = soln%psi_minus(i1,ind,:,2)
-  !  !call MUSCL_extrap(Vtmp, psiPtmp, psiMtmp, left, right)
-  !  left = soln%V(:,i1,j1)
-  !  right = soln%Vmms(:,i1,j1+1)
-  !  call flux_fun(left,right,nx,ny,soln%Feta(:,i1,j1))
-  !end do
+  i1 = i_high
+  do j1 = j_low,j_high
+    nx = grid%n_xi(i1+1,j1,1)
+    ny = grid%n_xi(i1+1,j1,2)
+    !ind = (/ ( k1,k1=i1-2,i1+1 ) /)
+    !Vtmp(1,:) = soln%V(ind(1),j1,:)
+    !Vtmp(2,:) = soln%Vmms(ind(2),j1,:)
+    !Vtmp(3,:) = soln%Vmms(ind(3),j1,:)
+    !Vtmp(4,:) = soln%Vmms(ind(4),j1,:)
+    !psiPtmp = soln%psi_plus(ind,j1,:,1)
+    !psiMtmp = soln%psi_minus(ind,j1,:,1)
+    !call MUSCL_extrap(Vtmp, psiPtmp, psiMtmp, left, right)
+    left = soln%V(:,i1,j1)
+    right = soln%Vmms(:,i1+1,j1)
+    call flux_fun(left,right,nx,ny,soln%Fxi(:,i1,j1))
+  end do
+  j1 = j_high
+  do i1 = i_low,i_high
+    nx = grid%n_eta(i1,j1+1,1)
+    ny = grid%n_eta(i1,j1+1,2)
+    !ind = (/ ( k1,k1=j1-2,j1+1 ) /)
+    !Vtmp(1,:) = soln%V(i1,ind(1),:)
+    !Vtmp(2,:) = soln%V(i1,ind(2),:)
+    !Vtmp(3,:) = soln%V(i1,ind(3),:)
+    !Vtmp(4,:) = soln%Vmms(i1,ind(4),:)
+    !psiPtmp = soln%psi_plus(i1,ind,:,2)
+    !psiMtmp = soln%psi_minus(i1,ind,:,2)
+    !call MUSCL_extrap(Vtmp, psiPtmp, psiMtmp, left, right)
+    left = soln%V(:,i1,j1)
+    right = soln%Vmms(:,i1,j1+1)
+    call flux_fun(left,right,nx,ny,soln%Feta(:,i1,j1))
+  end do
 
     call calc_time_step(grid,soln)
     call explicit_RK(grid,soln)
@@ -138,6 +139,8 @@ program main_program
    ! end if
     
     if (all(soln%Rnorm<tol)) then
+      write(*,*) k, soln%Rnorm
+      write(*,*) 'solution converged'
       exit
     end if
     
@@ -145,10 +148,16 @@ program main_program
   call calc_DE(soln,soln%DE,soln%DEnorm,cons)
   call output_res(soln,k)
   call output_soln(grid,soln,k)
+  write(42,*) i_high, 5, (soln%Rnorm(i),i=1,neq)
+  if (isMMS) then 
+    write(42,*) i_high, 1,(soln%DEnorm(i,1),i=1,neq)
+    write(42,*) i_high, 2,(soln%DEnorm(i,2),i=1,neq)
+    write(42,*) i_high, 0,(soln%DEnorm(i,3),i=1,neq)
+  end if
+  close(42)
   
   call grid_out(geometry_file,grid)
   call teardown_geometry(grid,soln)
-  close(50)
   write(*,*) 'Program End'
 end program main_program
   
