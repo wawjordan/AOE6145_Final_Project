6a568138958bf11d849898383eb0bf2742fa1570
Will Jordan <wajordan@vt.edu>
2021-06-09 09:31:08 -0400

    re-enabled cell-center calculation of source terms

--- src/types/soln_type.f90
+++ src/types/soln_type.f90
@@ -178,10 +178,10 @@ module soln_type
     
     call gauss_pts(xi,w,N)
     
-    !soln%Smms(1,:,:) = rmassconv(L,grid%xc,grid%yc)
-    !soln%Smms(2,:,:) = xmtmconv(L,grid%xc,grid%yc)
-    !soln%Smms(3,:,:) = ymtmconv(L,grid%xc,grid%yc)
-    !soln%Smms(4,:,:) = energyconv(gamma,L,grid%xc,grid%yc)
+    soln%Smms(1,:,:) = rmassconv(L,grid%xc,grid%yc)
+    soln%Smms(2,:,:) = xmtmconv(L,grid%xc,grid%yc)
+    soln%Smms(3,:,:) = ymtmconv(L,grid%xc,grid%yc)
+    soln%Smms(4,:,:) = energyconv(gamma,L,grid%xc,grid%yc)
     do j = jg_low, jg_high
       do i = ig_low, ig_high
         P = transpose( reshape( (/ &
@@ -194,11 +194,11 @@ module soln_type
         call gauss_quad(P,xi,xi,w,w,wrap_vvel_mms,soln%Vmms(3,i,j))
         call gauss_quad(P,xi,xi,w,w,wrap_press_mms,soln%Vmms(4,i,j))
         soln%Vmms(:,i,j) = soln%Vmms(:,i,j)/grid%V(i,j)
-        call gauss_quad(P,xi,xi,w,w,wrap_rmassconv,soln%Smms(1,i,j))
-        call gauss_quad(P,xi,xi,w,w,wrap_xmtmconv,soln%Smms(2,i,j))
-        call gauss_quad(P,xi,xi,w,w,wrap_ymtmconv,soln%Smms(3,i,j))
-        call gauss_quad(P,xi,xi,w,w,wrap_energyconv,soln%Smms(4,i,j))
-        soln%Smms(:,i,j) = soln%Smms(:,i,j)/grid%V(i,j)
+        !call gauss_quad(P,xi,xi,w,w,wrap_rmassconv,soln%Smms(1,i,j))
+        !call gauss_quad(P,xi,xi,w,w,wrap_xmtmconv,soln%Smms(2,i,j))
+        !call gauss_quad(P,xi,xi,w,w,wrap_ymtmconv,soln%Smms(3,i,j))
+        !call gauss_quad(P,xi,xi,w,w,wrap_energyconv,soln%Smms(4,i,j))
+        !soln%Smms(:,i,j) = soln%Smms(:,i,j)/grid%V(i,j)
       end do
     end do
     
