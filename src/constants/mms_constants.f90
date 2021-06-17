8ef3763c2a94cb07e25f0a0775945a5fe1701263
Will Jordan <wajordan@vt.edu>
2021-06-17 08:49:43 -0400

    Changed constants to evaluate subsonic test case

--- src/constants/mms_constants.f90
+++ src/constants/mms_constants.f90
@@ -9,12 +9,12 @@ module mms_constants
   real(prec), parameter :: rho0   = 1.0_prec
   real(prec), parameter :: rhox   = 0.15_prec
   real(prec), parameter :: rhoy   = -0.1_prec
-  real(prec), parameter :: uvel0  = 800.0_prec
+  real(prec), parameter :: uvel0  = 70.0_prec !800.0_prec
   real(prec), parameter :: uvelx  = 50.0_prec
-  real(prec), parameter :: uvely  = -30.0_prec
-  real(prec), parameter :: vvel0  = 800.0_prec
-  real(prec), parameter :: vvelx  = -75.0_prec
-  real(prec), parameter :: vvely  = 40.0_prec
+  real(prec), parameter :: uvely  = -7.0_prec !-30.0_prec
+  real(prec), parameter :: vvel0  = 90.0_prec !800.0_prec
+  real(prec), parameter :: vvelx  = -15.0_prec!-75.0_prec
+  real(prec), parameter :: vvely  = 8.5_prec  !40.0_prec
   real(prec), parameter :: wvel0  = 0.0_prec
   real(prec), parameter :: wvelx  = 0.0_prec
   real(prec), parameter :: wvely  = 0.0_prec
