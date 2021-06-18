901c184cbcbe34f86a06a9b5d2011b273d030fea
Will Jordan <wajordan@vt.edu>
2021-06-18 17:16:10 -0400

    made changes to allow BCs to be called more generically still need to fix/add wake cut, but inviscid wall appears to be working

--- src/types/bc_type.f90
+++ src/types/bc_type.f90
@@ -14,13 +14,14 @@ module bc_type
   public :: reflect_vec
   
   type, public :: bc_t
-    integer, dimension(2) :: i1, j1, dir
-    integer :: bc_id
+    integer :: bc_id, i_offset, j_offset
+    integer, dimension(2) :: i1, j1
+    integer, dimension(3) :: loop1, loop2
     real(prec), allocatable, dimension(:,:,:) :: Fxi, Feta
     real(prec), allocatable, dimension(:,:) :: nx, ny, &
                               rho, uvel, vvel, press, mach
   contains
-    procedure, public :: set_bc => set_bc_sub
+    procedure, public :: set_bc  => set_bc_sub
     procedure, public :: set_val => set_val_sub
     procedure, public :: enforce => enforce_sub
   end type bc_t
@@ -29,69 +30,73 @@ module bc_type
   
 contains
   
-  subroutine set_bc_sub(this,grid,ID,dir,i_low,i_high,j_low,j_high)
+  subroutine set_bc_sub(this,grid,ID,i_low,i_high,j_low,j_high,ij)
     implicit none
     class(bc_t) :: this
     type(grid_t), intent(in) :: grid
-    integer, dimension(2), intent(in) :: dir
     integer, intent(in) :: ID
     integer, intent(in) :: i_low, i_high, j_low, j_high
+    integer, intent(in) :: ij
     integer :: i,j
     this%i1 = (/ i_low, i_high /)
     this%j1 = (/ j_low, j_high /)
     this%bc_id = ID
-    this%dir = dir
     allocate( this%nx(i_low:i_high,j_low:j_high), &
               this%ny(i_low:i_high,j_low:j_high) )
-    if (dir(1)==0) then ! i-index constant
-    !write(*,*) 'i-index constant'
-      if (i_low<grid%i_low) then ! "left"-side
-      !write(*,*) 'left'
+    
+    select case(ij)
+      case(1) ! "left" (xi / i == constant)
         do i = i_low, i_high
           this%nx(i,:) = grid%n_xi(i_high,j_low:j_high,1)
           this%ny(i,:) = grid%n_xi(i_high,j_low:j_high,2)
         end do
-      else                       ! "right"-side
-      !write(*,*) 'right'
+        this%i_offset = -1
+        this%j_offset = 0
+        this%loop1 = (/j_low,j_high,1/)
+        this%loop2 = (/i_high,i_low,-1/)
+      case(2) ! "right" (xi / i == constant)
         do i = i_low, i_high
           this%nx(i,:) = grid%n_xi(i_low,j_low:j_high,1)
           this%ny(i,:) = grid%n_xi(i_low,j_low:j_high,2)
         end do
-      end if
-    else               ! j-index constant
-    !write(*,*) 'j-index constant'
-      if (j_low<grid%j_low) then ! "bottom"
-      !write(*,*) 'bottom'
+        this%i_offset = 1
+        this%j_offset = 0
+        this%loop1 = (/j_low,j_high,1/)
+        this%loop2 = (/i_low,i_high,1/)
+      case(3) ! "bottom" (eta / j == constant)
         do j = j_low, j_high
           this%nx(:,j) = grid%n_eta(i_low:i_high,j_high,1)
           this%ny(:,j) = grid%n_eta(i_low:i_high,j_high,2)
         end do
-      else                       ! "top"
-      !write(*,*) 'top'
+        this%i_offset = 0
+        this%j_offset = -1
+        this%loop1 = (/j_high,j_low,-1/)
+        this%loop2 = (/i_low,i_high,1/)
+      case(4) ! "top" (eta / j == constant)
         do j = j_low, j_high
           this%nx(:,j) = grid%n_eta(i_low:i_high,j_low,1)
           this%ny(:,j) = grid%n_eta(i_low:i_high,j_low,2)
         end do
-        !write(*,*) 'i, nx,ny'
-        !do i = i_low, i_high
-        !  write(*,*) i, this%nx(i,j_low), this%ny(i,j_low)
-        !end do
-      end if
-    end if
-    
-    select case(ID)
-    case(1:5)
+        this%i_offset = 0
+        this%j_offset = 1
+        this%loop1 = (/j_low,j_high,1/)
+        this%loop2 = (/i_low,i_high,1/)
+      case default
+    end select
+
+!    select case(ID)
+!    case(1:5)
       allocate( this%rho(  i_low:i_high,j_low:j_high),&
                 this%uvel( i_low:i_high,j_low:j_high),&
                 this%vvel( i_low:i_high,j_low:j_high),&
                 this%press(i_low:i_high,j_low:j_high) )
-    case(6)
-      allocate( this%Fxi (neq,i_low:i_high,j_low:j_high),&
-                this%Feta(neq,i_low:i_high,j_low:j_high) )
-    case(7)
-      allocate( this%mach(i_low:i_high,j_low:j_high) )
-    case default
-    end select
+!    case(6)
+!      allocate( this%Fxi (neq,i_low:i_high,j_low:j_high),&
+!                this%Feta(neq,i_low:i_high,j_low:j_high) )
+!    case(7)
+!      allocate( this%mach(i_low:i_high,j_low:j_high) )
+!    case default
+!    end select
     
     
   end subroutine set_bc_sub
@@ -113,115 +118,62 @@ contains
            soln%Vmms(3,this%i1(1):this%i1(2),this%j1(1):this%j1(2))
       this%press(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = &
            soln%Vmms(4,this%i1(1):this%i1(2),this%j1(1):this%j1(2))
-!    case(2)  ! Far field dirichlet
-!      this%rho(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = rho_inf
-!      this%uvel(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = &
-!                                        u_inf*cos((pi/180.0_prec)*alpha)
-!      this%vvel(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = &
-!                                        u_inf*sin((pi/180.0_prec)*alpha)
-!      this%press(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = p_inf
-!    case(3)  ! subsonic inflow
-!      d1 = this%dir(1)
-!      d2 = this%dir(2)
-!      do j = this%j1(1), this%j1(2)
-!        do i = this%i1(1), this%i1(2)
-!          this%rho(i,j)   = soln%V(1,i+d1,j+d2) + epsM*( &
-!                            soln%V(1,i+d1,j+d2) - soln%V(1,i+2*d1,j+2*d2) )
-!          this%uvel(i,j)  = soln%V(2,i+d1,j+d2) + epsM*( &
-!                            soln%V(2,i+d1,j+d2) - soln%V(2,i+2*d1,j+2*d2) )
-!          this%vvel(i,j)  = soln%V(3,i+d1,j+d2) + epsM*( &
-!                            soln%V(3,i+d1,j+d2) - soln%V(3,i+2*d1,j+2*d2) )
-!          this%press(i,j) = pb
-!        end do
-!      end do
-!    case(4)  ! supersonic inflow
-!      d1 = this%dir(1)
-!      d2 = this%dir(2)
-!      do j = this%j1(1), this%j1(2)
-!        do i = this%i1(1), this%i1(2)
-!          this%rho(i,j)   = soln%V(1,i+d1,j+d2) + epsM*( &
-!                            soln%V(1,i+d1,j+d2) - soln%V(1,i+2*d1,j+2*d2) )
-!          this%uvel(i,j)  = soln%V(2,i+d1,j+d2) + epsM*( &
-!                            soln%V(2,i+d1,j+d2) - soln%V(2,i+2*d1,j+2*d2) )
-!          this%vvel(i,j)  = soln%V(3,i+d1,j+d2) + epsM*( &
-!                            soln%V(3,i+d1,j+d2) - soln%V(3,i+2*d1,j+2*d2) )
-!          this%press(i,j) = pb
-!        end do
-!      end do
-    case(2)  ! subsonic inflow
-      d1 = this%dir(1)
-      d2 = this%dir(2)
-      do j = this%j1(1), this%j1(2)
-        do i = this%i1(1), this%i1(2)
-          !this%rho(i,j) = soln%Vmms(1,i,j)
-          this%uvel(i,j) = soln%Vmms(2,i,j)
-          this%vvel(i,j) = soln%Vmms(3,i,j)
-          this%press(i,j) = soln%Vmms(4,i,j)
-          !this%press(i,j) = soln%V(4,i+d1,j+d2) + epsM*( &
-          !                  soln%V(4,i+d1,j+d2) - soln%V(4,i+2*d1,j+2*d2) )
-        end do
-      end do
-!    case(3)  ! subsonic outflow
-!      d1 = this%dir(1)
-!      d2 = this%dir(2)
-!      do j = this%j1(1), this%j1(2)
-!        do i = this%i1(1), this%i1(2)
-!          this%rho(i,j) = soln%Vmms(1,i,j)
-!          !this%rho(i,j)   = soln%V(1,i+d1,j+d2) + epsM*( &
-!          !                  soln%V(1,i+d1,j+d2) - soln%V(1,i+2*d1,j+2*d2) )
-!          this%uvel(i,j) = soln%Vmms(2,i,j)
-!          this%vvel(i,j) = soln%Vmms(3,i,j)
-!          !this%uvel(i,j)  = soln%V(2,i+d1,j+d2) + epsM*( &
-!          !                  soln%V(2,i+d1,j+d2) - soln%V(2,i+2*d1,j+2*d2) )
-!          !this%vvel(i,j)  = soln%V(3,i+d1,j+d2) + epsM*( &
-!          !                  soln%V(3,i+d1,j+d2) - soln%V(3,i+2*d1,j+2*d2) )
-!          !this%press(i,j) = soln%Vmms(4,i,j)
-!          this%press(i,j) = soln%V(4,i+d1,j+d2) + epsM*( &
-!                            soln%V(4,i+d1,j+d2) - soln%V(4,i+2*d1,j+2*d2) )
-!        end do
-!      end do
+    case(2)  ! Far field dirichlet
+      this%rho(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = rho_inf
+      this%uvel(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = &
+                                        u_inf*cos((pi/180.0_prec)*alpha)
+      this%vvel(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = &
+                                        u_inf*sin((pi/180.0_prec)*alpha)
+      this%press(this%i1(1):this%i1(2),this%j1(1):this%j1(2)) = p_inf
     case(3)  ! subsonic outflow
-      d1 = this%dir(1)
-      d2 = this%dir(2)
-      do j = this%j1(1), this%j1(2)
-        do i = this%i1(1), this%i1(2)
-          this%rho(i,j)   = soln%V(1,i+d1,j+d2) + epsM*( &
-                            soln%V(1,i+d1,j+d2) - soln%V(1,i+2*d1,j+2*d2) )
-          this%uvel(i,j)  = soln%V(2,i+d1,j+d2) + epsM*( &
-                            soln%V(2,i+d1,j+d2) - soln%V(2,i+2*d1,j+2*d2) )
-          this%vvel(i,j)  = soln%V(3,i+d1,j+d2) + epsM*( &
-                            soln%V(3,i+d1,j+d2) - soln%V(3,i+2*d1,j+2*d2) )
+      d1 = this%i_offset
+      d2 = this%j_offset
+      do j = this%loop1(1),this%loop1(2),this%loop1(3)
+        do i = this%loop2(1),this%loop2(2),this%loop2(3)
+          this%rho(i,j)   = soln%V(1,i-d1,j-d2) + epsM*( &
+                            soln%V(1,i-d1,j-d2) - soln%V(1,i-2*d1,j-2*d2) )
+          this%uvel(i,j)  = soln%V(2,i-d1,j-d2) + epsM*( &
+                            soln%V(2,i-d1,j-d2) - soln%V(2,i-2*d1,j-2*d2) )
+          this%vvel(i,j)  = soln%V(3,i-d1,j-d2) + epsM*( &
+                            soln%V(3,i-d1,j-d2) - soln%V(3,i-2*d1,j-2*d2) )
           this%press(i,j) = p_inf
         end do
       end do
     case(4) ! supersonic outflow
-      d1 = this%dir(1)
-      d2 = this%dir(2)
-      do j = this%j1(1), this%j1(2)
-        do i = this%i1(1), this%i1(2)
-          this%rho(i,j)   = soln%V(1,i+d1,j+d2) + epsM*( &
-                            soln%V(1,i+d1,j+d2) - soln%V(1,i+2*d1,j+2*d2) )
-          this%uvel(i,j)  = soln%V(2,i+d1,j+d2) + epsM*( &
-                            soln%V(2,i+d1,j+d2) - soln%V(2,i+2*d1,j+2*d2) )
-          this%vvel(i,j)  = soln%V(3,i+d1,j+d2) + epsM*( &
-                            soln%V(3,i+d1,j+d2) - soln%V(3,i+2*d1,j+2*d2) )
-          this%press(i,j) = soln%V(4,i+d1,j+d2) + epsM*( &
-                            soln%V(4,i+d1,j+d2) - soln%V(4,i+2*d1,j+2*d2) )
+      d1 = this%i_offset
+      d2 = this%j_offset
+      do j = this%loop1(1),this%loop1(2),this%loop1(3)
+        do i = this%loop2(1),this%loop2(2),this%loop2(3)
+          this%rho(i,j)   = soln%V(1,i-d1,j-d2) + epsM*( &
+                            soln%V(1,i-d1,j-d2) - soln%V(1,i-2*d1,j-2*d2) )
+          this%uvel(i,j)  = soln%V(2,i-d1,j-d2) + epsM*( &
+                            soln%V(2,i-d1,j-d2) - soln%V(2,i-2*d1,j-2*d2) )
+          this%vvel(i,j)  = soln%V(3,i-d1,j-d2) + epsM*( &
+                            soln%V(3,i-d1,j-d2) - soln%V(3,i-2*d1,j-2*d2) )
+          this%press(i,j) = soln%V(4,i-d1,j-d2) + epsM*( &
+                            soln%V(4,i-d1,j-d2) - soln%V(4,i-2*d1,j-2*d2) )
         end do
       end do
     case(5)  ! slip wall w/ ghost cells
-      d1 = this%dir(1)
-      d2 = this%dir(2)
-      
-      do j = this%j1(1), this%j1(2)
-        do i = this%i1(2), this%i1(1),-1
-          call reflect_vec( soln%V(2,i+d1,j+d2), soln%V(3,i+d1,j+d2), &
+      d1 = this%i_offset
+      d2 = this%j_offset
+     !write(*,*) d1, d2
+     !write(*,*) this%i1(1), this%i1(2) 
+     !write(*,*) this%j1(1), this%j1(2)
+     ! do j = this%j1(1), this%j1(2)
+     !   do i = this%i1(2), this%i1(1),-1
+     !     write(*,*) this%nx(i,j), this%ny(i,j)
+     !   end do
+     ! end do
+     !stop
+      do j = this%loop1(1),this%loop1(2),this%loop1(3)
+        do i = this%loop2(1),this%loop2(2),this%loop2(3)
+          call reflect_vec( soln%V(2,i-d1,j-d2), soln%V(3,i-d1,j-d2), &
                               this%nx(i,j), this%ny(i,j), &
                               this%uvel(i,j), this%vvel(i,j) )
-          !write(*,*) soln%V(2,i+d1,j+d2), soln%V(3,i+d1,j+d2) ,this%uvel(i,j),this%vvel(i,j)
-          this%press(i,j) = soln%V(4,i+d1,j+d2) + zero*epsM*( &
-                            soln%V(4,i+d1,j+d2) - soln%V(4,i+2*d1,j+2*d2) )
-          this%rho(i,j)   = this%press(i,j)/( R_gas*soln%temp(i+d1,j+d2) )
+          this%press(i,j) = soln%V(4,i-d1,j-d2) + zero*epsM*( &
+                            soln%V(4,i-d1,j-d2) - soln%V(4,i-2*d1,j-2*d2) )
+          this%rho(i,j)   = this%press(i,j)/( R_gas*soln%temp(i-d1,j-d2) )
         end do
       end do
     case default
